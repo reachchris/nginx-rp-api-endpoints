7f71bfa83771df9e83f6509d0e2591475b8f4f7c
Christoffer Lantz <christoffer.lantz@ingka.com>
2021-03-03 16:30:12 +0100

    Pass through POST-data

--- servers/srv-1/index.js
+++ servers/srv-1/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
 app.get('/', function(req, res){
+    res.setHeader('SERVER-NODE', 1);
     res.send("Hello world 1!");
+});
+app.post('/', function(req, res){
+    res.setHeader('SERVER-NODE', 1);
+    res.setHeader('MESSAGE', 'This is your post data :)');
+    res.send(req.body);
 });
 
 app.listen(3000);
--- servers/srv-2/index.js
+++ servers/srv-2/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
 app.get('/', function(req, res){
+    res.setHeader('SERVER-NODE', 2);
     res.send("Hello world 2!");
+});
+app.post('/', function(req, res){
+    res.setHeader('SERVER-NODE', 2);
+    res.setHeader('MESSAGE', 'This is your post data :)');
+    res.send(req.body);
 });
 
 app.listen(3000);
